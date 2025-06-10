877d8849a50e9787435fe36cef51913c0ac02d29
Musbi <musbi700@gmail.com>
2025-06-10 23:07:32 +0000

    First Test

---
+++ name_function.py
@@ -0,0 +1,11 @@
+def get_formatted_name(first, last):
+    """_summary_
+    Generate a neatly formatted full name.
+
+    Args:
+        first (str): first name
+        last (str): last name
+    """
+
+    full_name = f"{first} {last}"
+    return full_name.title()
